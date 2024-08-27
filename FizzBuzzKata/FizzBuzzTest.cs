ab750c5115d75b27266a3f4c09ff852e286e0377
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-27 16:55:18 +0100

    🧪 Green: Test ShouldReturnFizzTo3 has passed.

--- FizzBuzzKata/FizzBuzzTest.cs
+++ FizzBuzzKata/FizzBuzzTest.cs
@@ -13,12 +13,26 @@ public class FizzBuzzTest
 
         result.Should().Be("1");
     }
+
+    [Fact(DisplayName = "Should return Fizz to 3")]
+    public void Should_return_Fizz_to_3()
+    {
+        var fizzbuzz = new FizzBuzz();
+
+        var result = fizzbuzz.Convert(3);
+
+        result.Should().Be("Fizz");
+    }
 }
 
 public class FizzBuzz
 {
     public string Convert(int number)
     {
+        if (number == 3)
+        {
+            return "Fizz";
+        }
         return number.ToString();
     }
 }
