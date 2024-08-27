be51e2f70ffb375cc06cbd55e74239ed3679371d
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-27 20:07:50 +0100

    🧪 Green: Test ShouldReturnFalseWhenYearIsNotMultipleOf4 has passed

--- LeapYearKata/LeapYearTest.cs
+++ LeapYearKata/LeapYearTest.cs
@@ -14,10 +14,28 @@ public class LeapYearTest
         result.Should().BeTrue();
     }
 
+    [Theory(DisplayName = "Should return false when year is not multiple of 4")]
+    [InlineData(3)]
+    [InlineData(1611)]
+    [InlineData(1995)]
+    [InlineData(2023)]
+    public void Should_return_false_when_year_is_not_multiple_of_4(int year)
+    {
+        var leapYear = new LeapYear();
+
+        var result = leapYear.IsLeapYear(year);
+
+        result.Should().BeFalse();
+    }
+
     public class LeapYear
     {
         public bool IsLeapYear(int year)
         {
+            if (year % 4 != 0)
+            {
+                return false;
+            }
             return true;
         }
     }
